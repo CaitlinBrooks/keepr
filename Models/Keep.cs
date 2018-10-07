e514310ca35f7cab7930c86b0f2b9ceba7e836b1
CaitlinCodes <caiit@me.com>
2018-10-07 16:52:01 -0600

    Keep

--- Models/Keep.cs
+++ Models/Keep.cs
@@ -4,5 +4,15 @@ using System.ComponentModel.DataAnnotations;
 
 namespace keepr.Models
 {
+  public class Keep
+  {
+    public string Img { get; set; }
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
 
+
+
+  }
 }
+// Needs to be marked private by utilizing a bool to switch from from true anfd false. 
