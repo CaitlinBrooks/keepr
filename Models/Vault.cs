373ab106f1748f6fc6f19fea331a654f4395826d
CaitlinCodes <caiit@me.com>
2018-10-07 17:06:52 -0600

    vault model somewhat complete -- need to ask

--- Models/Vault.cs
+++ Models/Vault.cs
@@ -7,5 +7,13 @@ namespace keepr.Models
     public int Id { get; set; }// Id of each individual vault name
     public string Name { get; set; } //vault name
     public string Description { get; set; } //vault description
+
+    public Vault(string userId, string name, string description) //needs these to create a vault
+    {
+      Name = name;
+      Description = description;
+      userId = userid; // ask about this
+    }
+    public Vault() { }
   }
 }
