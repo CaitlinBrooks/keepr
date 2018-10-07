32f03527249f3ac7a506232757d3fb8cf8d56169
CaitlinCodes <caiit@me.com>
2018-10-07 17:03:21 -0600

    vault model work

--- Models/Vault.cs
+++ Models/Vault.cs
@@ -1,1 +1,11 @@
 using System.ComponentModel.DataAnnotations;
+
+namespace keepr.Models
+{
+  public class Vault
+  {
+    public int Id { get; set; }// Id of each individual vault name
+    public string Name { get; set; } //vault name
+    public string Description { get; set; } //vault description
+  }
+}
