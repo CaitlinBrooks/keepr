62b650985e8d9c5d91492e22880156dddaf45c99
CaitlinCodes <caiit@me.com>
2018-10-08 15:17:20 -0600

    fixed keep model

--- Models/Keep.cs
+++ Models/Keep.cs
@@ -15,7 +15,7 @@ namespace keepr.Models
     [Required]
     [MaxLength(255)]
     public string Description { get; set; }
-    public bool IsPrivate { get; set; } // Use Tinyint and cast it with a bool to flip from logged out to logged in.
+    public bool IsPrivate { get; set; } // Uses Tinyint and casts it with a bool to flip from logged out to logged in.
     public int Views { get; set; }
     public int Keeps { get; set; }
     public int Shares { get; set; }
@@ -23,13 +23,11 @@ namespace keepr.Models
     public Keep(string userId, string Img, string name, string description) //needs these to create a keep
     {
       UserId = userId;
-      Img = img;
+      Img = Img;
       Name = name;
       Description = description;
     }
     public Keep() { }
-  }
-}
   }
 }
 
