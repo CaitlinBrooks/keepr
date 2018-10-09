818d25a7b65cb76dffc95ae5d29b8291d7c0faf7
CaitlinCodes <caiit@me.com>
2018-10-09 09:47:26 -0600

    vault keeps in keep model

--- Models/Keep.cs
+++ Models/Keep.cs
@@ -4,6 +4,13 @@ using System.ComponentModel.DataAnnotations;
 
 namespace keepr.Models
 {
+  public class VaultKeeps
+  {
+    public int Id { get; set; }
+    public string UserId { get; set; }
+    public int VaultId { get; set; }
+    public int KeepId { get; set; }
+  }
   public class Keep
   {
     public string UserId { get; set; }
