9cb93f67161d746fec321b34aae68870d91f4049
CaitlinCodes <caiit@me.com>
2018-10-10 14:23:08 -0600

    working on new vues

--- wwwsrc/src/views/Home.vue
+++ wwwsrc/src/views/Home.vue
@@ -1,22 +1,28 @@
 <template>
   <div class="home">
     <h1>Welcome Home</h1>
-    {{}}
+
   </div>
 </template>
 
 <script>
   export default {
     name: "home",
-    computed() {
+    computed: {
+      user() {
+        return this.$store.state.user;
+      },
+      components() {
+        getAllKeeps
+      },
+      mounted() {
+        this.$store.dispatch("getAllKeeps", this.$store.state.user.id);
+        this.$store.dispatch("getVaultsById", this.$store.state.user.id); // need this to be created in the store
 
-    },
-    mounted() {
-      this.$store.dispatch('getAllKeeps')
-      //blocks users not logged in
-      if (!this.$store.state.user.id) {
-        this.$router.push({ name: "login" });
+        //blocks users not logged in
+        if (!this.$store.state.user.id) {
+          this.$router.push({ name: "login" });
+        }
       }
-    }
-  };
+    };
 </script>
