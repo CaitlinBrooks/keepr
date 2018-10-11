affe353beab269a8486a640ba0cc2394db49c1c3
CaitlinCodes <caiit@me.com>
2018-10-11 10:19:44 -0600

    keeps vue

---
+++ wwwsrc/src/components/Keeps.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="results">
+    <h1>Keeps</h1>
+    <div class="keepCard" v-if="keep.isPrivate == 0">
+      <div v-for="keep in keeps" :key="keep.id">
+        <img :src="keep.img" class="keepCard" alt="keep.name">
+        <p>Title: {{keep.title}}</p>
+        <p>Description: {{keep.description}}</p>
+      </div>
+    </div>
+  </div>
+  <!-- reference Vue-Music for this card -->
+</template>
+
+<script>
+  export default {
+    computed: {
+      keeps() {
+        return this.$store.state.keeps;
+      }
+    }
+  };
+</script>
+
+<style>
+</style>
