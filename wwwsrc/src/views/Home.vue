250ce1d253db574861f0eaa643c61336122b5d6a
CaitlinCodes <caiit@me.com>
2018-10-13 16:11:42 -0600

    vault in Home.vue

--- wwwsrc/src/views/Home.vue
+++ wwwsrc/src/views/Home.vue
@@ -23,7 +23,7 @@
       UserVaults
     },
     mounted() {
-      this.$store.dispatch("getVaultsById", this.user.id);
+      this.$store.dispatch("getVaultById", this.user.id);
       this.$store.dispatch("getKeepsByUserId", this.user.id);
 
       //blocks users not logged in
