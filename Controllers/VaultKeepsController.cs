626d7a56544b7887dcc1e7a2521dd4ba8cb3d8fa
CaitlinCodes <caiit@me.com>
2018-10-13 15:52:09 -0600

    maybe

--- Controllers/VaultKeepsController.cs
+++ Controllers/VaultKeepsController.cs
@@ -1,60 +1,64 @@
-using System;
-using System.Collections.Generic;
-using keepr.Models;
-using keepr.Repositories;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+// using System;
+// using System.Collections.Generic;
+// using keepr.Models;
+// using keepr.Repositories;
+// using Microsoft.AspNetCore.Authorization;
+// using Microsoft.AspNetCore.Mvc;
 
-namespace keepr.Controllers
-{
-  [Route("api/[controller]")]
-  [ApiController]
-  public class VaultKeepsController : Controller
-  {
-    VaultKeepsRepository _repo;
-    public KeepsController(KeepsRepository repo)
-    {
-      _repo = repo;
-    }
+// IS THIS ALL CORRECT? WORTH SPLITTING THEM OUT FROM KEEPS?
 
-    [HttpGet]
-    public IEnumerable<Keep> Get()
-    {
-      var keeps = _repo.GetAll();
-      return keeps;
-    }
 
-    [HttpGet("/vaultkeeps/{id}")]
-    public IEnumerable<Keep> Get(int id)
-    {
-      return _repo.GetbyVaultId(id);
-    }
 
-    [HttpGet("{userid}")]
-    public IEnumerable<Keep> Get(string userid)
-    {
-      userid = HttpContext.User.Identity.Name;
-      return _repo.GetById(userid);
-    }
+// namespace keepr.Controllers
+// {
+//   [Route("api/[controller]")]
+//   [ApiController]
+//   public class VaultKeepsController : Controller
+//   {
+//     VaultKeepsRepository _repo;
+//     public KeepsController(KeepsRepository repo)
+//     {
+//       _repo = repo;
+//     }
 
-    [Authorize]
-    [HttpPost]
-    public Keep Post([FromBody] Keep keep)
-    {
-      if (ModelState.IsValid)
-      {
-        keep = new Keep(keep.UserId, keep.Img, keep.Name, keep.Description);
-        return _repo.Create(keep);
-      }
-      throw new Exception("INVALID KEEP");
-    }
+//     [HttpGet]
+//     public IEnumerable<Keep> Get()
+//     {
+//       var keeps = _repo.GetAll();
+//       return keeps;
+//     }
 
-    [HttpDelete("{{id}}")]
-    public void Delete(int keepId)
-    {
-      _repo.Delete(keepId);
-    }
+//     [HttpGet("/vaultkeeps/{id}")]
+//     public IEnumerable<Keep> Get(int id)
+//     {
+//       return _repo.GetbyVaultId(id);
+//     }
 
-  }
+//     [HttpGet("{userid}")]
+//     public IEnumerable<Keep> Get(string userid)
+//     {
+//       userid = HttpContext.User.Identity.Name;
+//       return _repo.GetById(userid);
+//     }
 
-}
+//     [Authorize]
+//     [HttpPost]
+//     public Keep Post([FromBody] Keep keep)
+//     {
+//       if (ModelState.IsValid)
+//       {
+//         keep = new Keep(keep.UserId, keep.Img, keep.Name, keep.Description);
+//         return _repo.Create(keep);
+//       }
+//       throw new Exception("INVALID KEEP");
+//     }
+
+//     [HttpDelete("{{id}}")]
+//     public void Delete(int keepId)
+//     {
+//       _repo.Delete(keepId);
+//     }
+
+//   }
+
+// }
