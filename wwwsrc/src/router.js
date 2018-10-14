a27b023c3915a23e10b8598ea500d93be0dc3e3a
CaitlinCodes <caiit@me.com>
2018-10-14 13:39:24 -0600

    router updates

--- wwwsrc/src/router.js
+++ wwwsrc/src/router.js
@@ -5,7 +5,11 @@ import Home from './views/Home.vue'
 // @ts-ignore
 import Login from './views/Login.vue'
 // @ts-ignore
-import VaultKeeps from './views/VaultKeeps.vue'
+import UserVaults from './components/UserVaults.vue'
+// @ts-ignore
+import Vault from './components/Vault.vue'
+// @ts-ignore
+import Keeps from './components/Keeps.vue'
 
 Vue.use(Router)
 
@@ -22,9 +26,24 @@ export default new Router({
       component: Login
     },
     {
-      path: '/',
-      name: 'vaultkeeps',
-      component: VaultKeeps
+      path: '/uservaults',
+      name: 'uservaults',
+      component: UserVaults
     },
+    {
+      path: '/vaults/:vaultId',
+      name: 'vault',
+      component: Vault
+    },
+    {
+      path: '/keeps',
+      name: 'keeps',
+      component: Keeps
+    },
+    {
+      path: '*',
+      redirect: '/'
+    },
+
   ]
 })
